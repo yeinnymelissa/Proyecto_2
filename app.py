6ce7f0155510fb3cb29039d7f69c15e0f1c70a29
yeinnymelissa <82351981+yeinnymelissa@users.noreply.github.com>
2021-04-29 10:09:31 -0600

    prueba6

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def obtener_usuario():
 
 @app.route('/')
 def index():
-    return index.html
+    return 'holaaaa'
 
 if __name__ == '__main__':
     app.run(threaded=True, debug=True, port=4000)
