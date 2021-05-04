5eff974e9d8f4f0be5cee8dbbf1fd2766adc50be
yeinnymelissa <82351981+yeinnymelissa@users.noreply.github.com>
2021-05-03 18:34:05 -0600

    prueba

---
+++ CRUD_Doctor.py
@@ -0,0 +1,28 @@
+from usuario import Usuario
+
+class CRUD_Usuario:
+    def __init__(self):
+        self.usuarios = []
+        self.usuarios.append(Usuario('0', 'Ariel', 'Bautista', "", "M", "admin", "1234", "", "0"))
+
+    def agregar(self, nombre, apellido, fecha_nac, sexo, nom_usuario, contraseña, telefono, puesto):
+        id = len(self.usuarios)
+        self.usuarios.append(Usuario(id, nombre, apellido, fecha_nac, sexo, nom_usuario, contraseña, telefono, puesto))
+        return id
+
+    def obtener_id(self, id):
+        for usuario in self.usuarios:
+            if usuario.id == id:
+                return usuario.dump()
+
+        return None
+
+    def login(self, nom_usuario, contraseña):
+        for usuario in self.usuarios:
+            if usuario.nom_usuario == nom_usuario and usuario.contraseña == contraseña:
+                return usuario.dump()
+        
+        return None
+
+    def obtener_general(self):
+        return [usuario.dump() for usuario in self.usuarios]
